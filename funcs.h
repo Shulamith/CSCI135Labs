d37bc251a37188e01f9ec7a781d250d2d8a5e16b
Mike Zamansky <zamansky@gmail.com>
2019-04-12 13:46:43 -0400

    z

---
+++ funcs.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+
+// sample signature
+std::string greet(std::string name);
+
+// provided stuff
+#define MAX_H 512
+#define MAX_W 512
+
+void readImage(std::string filename, int image[MAX_H][MAX_W], int &height, int &width);
+void writeImage(std::string filename,int image[MAX_H][MAX_W], int height, int width);
+
+// Write your function signatures / prototypes here
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+#include "funcs.h"
+
+int main()
+{
+
+  // sample code
+
+
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage("test_image.pgm",img, h, w); // read it from the file "inImage.pgm"
+
+  // h and w were passed by reference and
+  // now contain the dimensions of the picture
+  // and the 2-dimesional array img contains the image data
+
+  // Now we can manipulate the image the way we like
+  // for example we copy its contents into a new array
+  int out[MAX_H][MAX_W];
+
+  for(int row = 0; row < h; row++) {
+    for(int col = 0; col < w; col++) {
+      out[row][col] = img[row][col];
+    }
+  }
+
+  // and save this new image to file "outImage.pgm"
+  writeImage("test_out.pgm",out, h, w);
+
+return 0;
+}
