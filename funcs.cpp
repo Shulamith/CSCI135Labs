4ab4619f02d7175331cb608119e8d0cebb809288
Shulamith <shulamit.dashevsky@gmail.com>
2019-04-18 09:15:09 -0400

    done w c

--- funcs.cpp
+++ funcs.cpp
@@ -76,18 +76,39 @@ void writeImage(std::string filename,int image[MAX_H][MAX_W], int height, int wi
   return;
 }
 
-int invert(int color){ //this func shud recieves just one color value //not sure if it was best to split this
-return (255-color);
-}
-int invert_image(int img, int height, int width){
+// int invert(int color){ //this func shud recieves just one color value //not sure if it was best to split this
+// return (255-color);
+// }
+int invert_general(int img, int starting_row, int starting_col, int ending_row, int ending_col){ //refactoring function
   int inverted_img[height][width];
-  for (int row= 0; i < height; row++) {
-    for (int col= 0; i < width; col++) {
-      inverted_img[row][col] = invert(img[row][col]);
-
+  for (int row=starting_row; i < ending_row; row++) {
+    for (int col= starting_col; i < ending; col++) {
+      inverted_img[row][col] = 255-(img[row][col]);
     }
-    /* code */
   }
+  return inverted_img;
+}
+int invert(int img, int height, int width){
+  int inverted_img[height][width];
+  inverted_img = invert_general(img, 0,0,height,width);
+  return inverted_img;
+}
+int invert_half(int img, int height, int width){
+  int inverted_img[height][width];
+  inverted_img = invert_general(img,0,width/2,height,width);
+  return inverted_img;
+}
+int white_box(int img, int height, int width){
+  int box_img[height][width];
+  int starting_row = height/4;
+  int starting_col = width/4;
+  int ending_row = (3/4)*height;
+  int ending_col = (3/4)*
+  box_img = invert_general(img,starting_row,starting_col,ending_row,ending_col);
+  return box_img;
+}
+int frame(int img,int height,int width){
+  int framed[height][width];s
 }
 
 
